06ae15ea2942312819615654ac670e927f141e7c
lon <alfardproject@gmail.com>
2019-01-18 10:55:11 -0800

    Add files via upload

---
+++ SaveLoadInBinarySystem/Player.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : MonoBehaviour
+{
+    public int level = 3;
+    public int health = 10;
+}
---
+++ SaveLoadInBinarySystem/SaveSystemBinary.cs
@@ -0,0 +1,38 @@
+﻿//storing data
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public static class SaveSystemBinary 
+{
+
+    private static string path = Application.persistentDataPath + "/player.fun";
+
+
+    public static void SavePlayer(Player player)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        PlayerData data = new PlayerData(player);
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static PlayerData LoadPlayer() {
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+            formatter.Deserialize(stream);
+            PlayerData data = formatter.Deserialize(stream) as PlayerData;
+            stream.Close();
+            return data;
+        }
+        else {
+            Debug.LogError("Save file not found in" + path);
+            return null;
+        }
+    }
+}
