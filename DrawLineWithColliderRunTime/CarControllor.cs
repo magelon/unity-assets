9d7825d5b3dfb07896f16002c72131eb66b04cf5
lon <alfardproject@gmail.com>
2019-01-18 11:04:29 -0800

    Add files via upload

---
+++ DrawLineWithColliderRunTime/CarControllor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarControllor : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(1)) {
+            rb.bodyType = RigidbodyType2D.Dynamic; 
+        }
+    }
+}
---
+++ DrawLineWithColliderRunTime/LineCreator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LineCreator : MonoBehaviour
+{
+    public GameObject linePrefab;
+
+    DrawLineWithCollider activeLine;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject lineGO = Instantiate(linePrefab);
+            activeLine = lineGO.GetComponent<DrawLineWithCollider>();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            activeLine = null;
+        }
+
+        if (activeLine != null)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            activeLine.UpdateLine(mousePos);
+        }
+
+    }
+}
