d0b4ebeceba73fb58d50e7974e7b0ab8cc66cedb
lon <alfardproject@gmail.com>
2019-01-18 10:56:16 -0800

    Add files via upload

---
+++ springJointDragReleaseBall/drag.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drag : MonoBehaviour
+{
+    private bool isPressed = false;
+    public Rigidbody2D rb;
+    public float timeing=.15f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPressed)
+        {
+            rb.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+
+    IEnumerator Release() {
+        yield return new WaitForSeconds(timeing);
+        GetComponent<SpringJoint2D>().enabled=false;
+    }
+
+    private void OnMouseDown()
+    {
+        isPressed = true;
+        rb.isKinematic = true;
+    }
+    private void OnMouseUp()
+    {
+        isPressed = false;
+        rb.isKinematic = false;
+        StartCoroutine(Release());
+    }
+}
