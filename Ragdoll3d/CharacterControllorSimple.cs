eb84c5d1223c16974cefc381314130b5c16489a6
lon <alfardproject@gmail.com>
2019-01-19 16:49:16 -0800

    Add files via upload

---
+++ Ragdoll3d/CharacterControllorSimple.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControllorSimple : MonoBehaviour {
+
+    public float speed = 10.0f;
+    public float rotationSpeed = 100.0f;
+    //animator from the cube which your leg will be imitating
+    public Animator a;
+    //public GameObject cube;
+    bool walking = false;
+    // Use this for initialization
+    void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetAxis("Vertical")!=0 && walking!=true) {
+            walking = true;
+            a.SetBool("walking", true);
+            
+        }
+        if(Input.GetAxis("Vertical")==0 && walking!=false)
+        {
+            walking = false;
+            a.SetBool("walking", false);
+            
+        }
+        // Get the horizontal and vertical axis.
+        // By default they are mapped to the arrow keys.
+        // The value is in the range -1 to 1
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        // Make it move 10 meters per second instead of 10 meters per frame...
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        // Move translation along the object's y-axis
+        transform.Translate(0, 0, translation);
+
+        // Rotate around our y-axis
+        transform.Rotate(-rotation, 0, 0);
+    }
+}
---
+++ Ragdoll3d/piernasmov.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//this script response for leg animation
+public class piernasmov : MonoBehaviour {
+
+    public HingeJoint hj;
+    public Transform objetivo;
+    public bool invertido;
+    
+    // Suscribe Edsonxn Channel On Youtube!!
+    //for more tutorials
+   
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+
+    //imitating the cube swaying animation to perform a walking animation
+    void Update()
+    {
+        JointSpring js = hj.spring;
+        js.targetPosition = objetivo.localEulerAngles.x;
+        if (js.targetPosition > 180)
+            js.targetPosition = js.targetPosition - 360;
+        js.targetPosition = Mathf.Clamp(js.targetPosition, hj.limits.min + 5, hj.limits.max - 5);
+        if (invertido)
+        {
+            js.targetPosition = js.targetPosition * -1;
+        }
+        hj.spring = js;
+    }
+    
+}
