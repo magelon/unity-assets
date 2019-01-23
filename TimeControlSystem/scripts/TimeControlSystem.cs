02d2f1103a20886b4f6e92b7e233771d044489ba
lon <alfardproject@gmail.com>
2019-01-23 11:16:04 -0800

    Add files via upload

---
+++ TimeControlSystem/scripts/TimeControlSystem.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeControlSystem : MonoBehaviour
+{
+    public float turnDuration = 1f;
+    public float fastForwardMultiplier = 5f;
+    public bool paused;
+    public bool fastForward;
+
+    public delegate void OnTimeAdvanceHandler();
+    public static event OnTimeAdvanceHandler OnTimeAdvance;
+
+    private float advanceTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        advanceTimer = turnDuration;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!paused)
+        {
+            advanceTimer -= Time.deltaTime * (fastForward ? fastForwardMultiplier : 1f);
+            if (advanceTimer <= 0)
+            {
+                advanceTimer += turnDuration;
+                OnTimeAdvance?.Invoke();
+            }
+        }
+    }
+    //do this with a state mechine future
+    public void Step() {
+        OnTimeAdvance?.Invoke();
+    }
+    public void Pause() {
+        paused = true;
+        fastForward = false;
+    }
+    public void Play() {
+        paused = false;
+        fastForward = false;
+    }
+    public void FastForward() {
+        paused = false;
+        fastForward = true;
+    }
+}
---
+++ TimeControlSystem/scripts/TurnCounter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TurnCounter : MonoBehaviour
+{
+    public Text turnDispaly;
+    int turn = 0;
+    // Start is called before the first frame update
+    private void OnEnable()
+    {
+        //Subscribe to time control system
+        TimeControlSystem.OnTimeAdvance += Advance;
+    }
+    private void OnDisable()
+    {
+        //unsubscribe from time control system
+        TimeControlSystem.OnTimeAdvance -= Advance;
+    }
+   public void Advance() {
+        turn++;
+        turnDispaly.text = string.Format("Turn: {0}", turn);
+   }
+}
