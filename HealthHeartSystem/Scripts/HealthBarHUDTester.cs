ca001f0e8dacd2258fa8f31a4ff4050bb14e2bed
lon <alfardproject@gmail.com>
2019-01-29 12:20:24 -0800

    Add files via upload

---
+++ HealthHeartSystem/Scripts/HealthBarHUDTester.cs
@@ -0,0 +1,23 @@
+﻿/*
+ *  Author: ariel oliveira [o.arielg@gmail.com]
+ */
+ //this call this script to modify play or enemy health
+using UnityEngine;
+
+public class HealthBarHUDTester : MonoBehaviour
+{
+    public void AddHealth()
+    {
+        PlayerStats.Instance.AddHealth();
+    }
+
+    public void Heal(float health)
+    {
+        PlayerStats.Instance.Heal(health);
+    }
+
+    public void Hurt(float dmg)
+    {
+        PlayerStats.Instance.TakeDamage(dmg);
+    }
+}
