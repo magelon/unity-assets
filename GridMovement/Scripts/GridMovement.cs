cfa8eea7541c8b36aab51b8b12da06d51b8b9b07
lon <alfardproject@gmail.com>
2019-02-03 23:43:43 -0800

    Add files via upload

---
+++ GridMovement/Scripts/GridMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//set camera to orthographic
+public class GridMovement : MonoBehaviour
+{
+    float moveSpeed = 1;
+    Vector3 mousePosition;
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = new Vector2(Mathf.Round(mousePosition.x)
+            ,Mathf.Round(mousePosition.y));
+    }
+}
