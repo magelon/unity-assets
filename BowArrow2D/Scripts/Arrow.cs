d1980c6274f25d4296fcc9de9bea3952d869fd54
lon <alfardproject@gmail.com>
2019-02-03 18:41:24 -0800

    Add files via upload

---
+++ BowArrow2D/Scripts/Arrow.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arrow : MonoBehaviour
+{
+	public float flyTime;
+	//public Collider childCollider;
+	
+	private bool flying = true;
+	private float stopTime;
+	private Transform anchor;
+	
+	void Start()
+	{
+		this.stopTime = Time.time + this.flyTime;
+	}
+	
+	void Update ()
+	{
+
+        //this part of update is only executed, if a rigidbody is present
+        // the rigidbody is added when the arrow is shot (released from the bowstring)
+        if (transform.GetComponent<Rigidbody2D>() != null)
+        {
+            // do we fly actually?
+            if (GetComponent<Rigidbody2D>().velocity != Vector2.zero)
+            {
+                // get the actual velocity
+                Vector3 vel = GetComponent<Rigidbody2D>().velocity;
+                // calc the rotation from x and y velocity via a simple atan2
+                float angleZ = Mathf.Atan2(vel.y, vel.x) * Mathf.Rad2Deg;
+                float angleY = Mathf.Atan2(vel.z, vel.x) * Mathf.Rad2Deg;
+                // rotate the arrow according to the trajectory
+                transform.eulerAngles = new Vector3(0, -angleY, angleZ);
+            }
+        }
+
+        if (this.stopTime <= Time.time && this.flying)
+		{
+			GameObject.Destroy(gameObject);	
+		}
+		
+		if(this.flying)
+		{
+			//this.rotate();	
+		} 
+		else if(this.anchor != null)
+		{
+			this.transform.position = anchor.transform.position;
+			this.transform.rotation = anchor.transform.rotation;
+		}
+		
+	}
+	
+	void OnCollisionEnter2D(Collision2D collision)
+	{	
+		if(this.flying)
+		{
+			this.flying = false;
+			this.transform.position = collision.contacts[0].point;
+			//this.childCollider.isTrigger = true;
+				
+			GameObject anchor = new GameObject("ARROW_ANCHOR");
+			anchor.transform.position = this.transform.position;
+			anchor.transform.rotation = this.transform.rotation;
+			anchor.transform.parent = collision.transform;
+			this.anchor = anchor.transform;
+			
+			Destroy(GetComponent<Rigidbody2D>());
+			//collision.gameObject.SendMessage("arrowHit", SendMessageOptions.DontRequireReceiver);
+		}
+	}
+}
---
+++ BowArrow2D/Scripts/Bow.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bow : MonoBehaviour
+{
+	public float maxPower;
+	public float powerIncreaseRate;
+	public GameObject arrow;
+	//public Collider origin;
+    public Collider2D origin;
+	
+	private bool pulling = false;
+	private float power = 0;
+	
+	void OnGUI()
+	{
+		GUI.Box(new Rect(0, 0, 100, 50), this.power.ToString());	
+	}
+	
+	void Update ()
+	{
+
+        Vector3 forward = transform.TransformDirection(Vector3.right)*3;
+        Debug.DrawRay(transform.position, forward, Color.green);
+
+        if (Input.GetMouseButtonDown(0))
+		{
+			this.startPull();
+		}
+		
+		if(Input.GetMouseButtonUp(0))
+		{
+			this.stopPull();
+		}
+		
+		if(pulling){
+			power += powerIncreaseRate;
+			
+			if(power > maxPower) power = maxPower;
+		}
+	}
+	
+	private void startPull()
+	{
+		this.pulling = true;
+		this.power = 0;
+	}
+	
+	private void stopPull()
+	{
+		this.pulling = false;
+		this.fire();
+	}
+	
+	private void fire()
+	{
+		GameObject arrow = this.spawnArrow();
+
+
+        arrow.GetComponent<Rigidbody2D>()
+            .AddForce(Quaternion
+            .Euler(new Vector3(transform.rotation.eulerAngles.x, 
+            transform.rotation.eulerAngles.y,
+            transform.rotation.eulerAngles.z))
+            * new Vector3(25f * power, 0, 0), ForceMode2D.Force);
+
+
+        //arrow.GetComponent<Rigidbody2D>().AddRelativeForce(Vector2.right * this.power);
+		
+	}
+	
+	private GameObject spawnArrow()
+	{
+		Vector3 start = transform.position;
+		Quaternion rotation = transform.rotation;
+		
+		GameObject obj = (GameObject)GameObject.Instantiate(this.arrow, start, rotation);
+		Arrow arrow = obj.GetComponent<Arrow>();
+        if (arrow != null) {
+            Physics2D.IgnoreCollision(this.origin, arrow.GetComponent<Collider2D>());
+        }
+       
+		//Physics.IgnoreCollision(this.origin, arrow.childCollider);
+		
+		return obj;
+	}
+}
