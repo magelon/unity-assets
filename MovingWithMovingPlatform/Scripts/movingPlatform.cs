75ecd998a89550f9235d41cb3dc6dda2e51fce18
lon <alfardproject@gmail.com>
2019-02-06 00:22:31 -0800

    Add files via upload

---
+++ MovingWithMovingPlatform/Scripts/movingPlatform.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movingPlatform : MonoBehaviour
+{
+    private Vector3 velocity;
+    private bool moving;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        collision.collider.transform.SetParent(transform);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        collision.collider.transform.SetParent(null);
+    }
+}
