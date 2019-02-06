2a422903789616b1ba8f40ead436d2c013dcb072
lon <alfardproject@gmail.com>
2019-02-06 15:46:03 -0800

    Add files via upload

---
+++ RagDoll2D/Scripts/ImitateRag.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImitateRag : MonoBehaviour
+{
+ 
+    public Transform objetivo;
+    public bool invertido;
+
+
+    //imitating the cube swaying animation to perform a walking animation
+    void Update()
+    {
+
+       transform.eulerAngles= objetivo.localEulerAngles;
+       if (invertido)
+        {
+            transform.eulerAngles = -objetivo.localEulerAngles;
+        }
+
+    }
+}
