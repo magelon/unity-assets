98af77fdaab9bc38d9d3fd910a2e7bfb40c6cbfb
lon <alfardproject@gmail.com>
2019-02-10 15:10:24 -0800

    Add files via upload

---
+++ SlowMotionFixed/slowMotion.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class slowMotion : MonoBehaviour
+{
+    private float slow = 0.1f;
+    private float normTime = 1.0f;
+    private bool doslow = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Time.timeScale = normTime;
+            Time.fixedDeltaTime = 0.02f * Time.timeScale;
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            Time.timeScale = slow;
+            Time.fixedDeltaTime = 0.02f * Time.timeScale;
+        }
+    }
+}
