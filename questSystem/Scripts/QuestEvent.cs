6b93cf1d9441481d6b09e7318ac03651e4b4d13a
lon <alfardproject@gmail.com>
2019-02-21 14:50:19 -0800

    quest System
    delete event component if  event component already

---
+++ questSystem/Scripts/QuestEvent.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class QuestEvent 
+{
+   public enum EventStatus
+    {
+        WAITING,CURRENT,DONE
+    };
+    //waiting :not wake
+    //current :wake
+    //done
+    public string name;
+    public string description;
+    public string id;
+    public int order=-1;
+    public EventStatus status;
+    public QuestButton button;
+
+    public List<QuestPath> pathlist = new List<QuestPath>();
+
+    public QuestEvent(string n,string d)
+    {
+        id = Guid.NewGuid().ToString();
+        name = n;
+        description = d;
+        status = EventStatus.WAITING;
+    }
+
+    public void UpdateQuestEvent(EventStatus es)
+    {
+        status = es;
+        button.UpadteButton(es);
+    }
+    public string GetId()
+    {
+        return id;
+    }
+
+}
---
+++ questSystem/Scripts/QuestManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestManager : MonoBehaviour
+{
+    public Quest quest = new Quest();
+    public GameObject questPrintBox;
+    public GameObject buttonPrefab;
+    void Start()
+    {
+        //creat each event
+        QuestEvent a = quest.AddQuestEvent("test1", "desc1");
+        QuestEvent b = quest.AddQuestEvent("test2", "desc2");
+        QuestEvent c = quest.AddQuestEvent("test3", "desc3");
+        QuestEvent d = quest.AddQuestEvent("test4", "desc4");
+        QuestEvent e = quest.AddQuestEvent("test5", "desc5");
+
+        //define the paths between the events
+        quest.AddPath(a.GetId(), b.GetId());
+        quest.AddPath(b.GetId(), c.GetId());
+        quest.AddPath(b.GetId(), d.GetId());
+        quest.AddPath(c.GetId(), e.GetId());
+        quest.AddPath(d.GetId(), e.GetId());
+
+        quest.BFS(a.GetId());
+        QuestButton button = CreatButton(a).GetComponent<QuestButton>();
+        button = CreatButton(b).GetComponent<QuestButton>();
+        button = CreatButton(c).GetComponent<QuestButton>();
+        button = CreatButton(d).GetComponent<QuestButton>();
+        button = CreatButton(e).GetComponent<QuestButton>();
+        quest.PrintPath();
+    }
+    GameObject CreatButton(QuestEvent e)
+    {
+        GameObject b = Instantiate(buttonPrefab);
+        b.GetComponent<QuestButton>().Setup(e, questPrintBox);
+        if (e.order == 1)
+        {
+            b.GetComponent<QuestButton>().UpadteButton(QuestEvent.EventStatus
+            .CURRENT);
+            e.status = QuestEvent.EventStatus.CURRENT;
+        }return b;
+    }
+    public void UpdateQuestsOnCompletion(QuestEvent e)
+    {
+        foreach(QuestEvent n in quest.questEvents)
+        {
+            if (n.order == (e.order + 1))
+            {
+                n.UpdateQuestEvent(QuestEvent.EventStatus.CURRENT);
+            }
+        }
+    }
+}
